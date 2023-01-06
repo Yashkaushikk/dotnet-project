d50fde891eaf1ff82cfc00ddc16a95f586f4aadd
Yash Kaushik <68856962+Yashkaushikk@users.noreply.github.com>
2023-01-06 23:51:27 +0530

    Add files via upload

---
+++ Basicapplication/Basicapplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basicapplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
